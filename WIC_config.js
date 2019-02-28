06af8d52db0af4d7f374b2afe1a30174acd3fcef
Amelia Berumen <ameliaberumen@amelias-mbp.attlocal.net>
2019-02-27 22:51:21 -0800

    wic

--- WIC_config.js
+++ WIC_config.js
@@ -1,2 +1,5 @@
 // API key
 const API_KEY = "pk.eyJ1IjoiYWJlcnVtZW4xMyIsImEiOiJjanNodzN3bGoxZ2luNDlxZ2ZoNDJ5N3BvIn0.oHhw0sTwBrKDinymLaZkog";
+
+
+//pk.eyJ1IjoiYWJlcnVtZW4xMyIsImEiOiJjanNodzN3bGoxZ2luNDlxZ2ZoNDJ5N3BvIn0.oHhw0sTwBrKDinymLaZkog
